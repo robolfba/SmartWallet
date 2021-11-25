5c89f1e6bf9a823b47b0d27784a43da0c3d593bb
unknown <robolfba@gmail.com>
2021-11-25 19:06:25 -0300

    Se aplican middlewares al archivo server.js

--- api/config/server.js
+++ api/config/server.js
@@ -1,10 +1,14 @@
+// CONFIGURACION GENERAL DEL SERVIDOR, aca van los middlewares y demas
+const routes = require("../src/routes");
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
 
 app.set("PORT", process.env.PORT || 3001);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(morgan("dev"));
+app.use("/", routes);
 
 module.exports = app;
