3c43f7d840cadfcfd0e11676966e17126f408a42
unknown <robolfba@gmail.com>
2021-11-25 19:08:17 -0300

    Se define estructura para probar conexion de base de datos con el servidor

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const app = require("./config/server.js");
-
+const { conn } = require("./src/db.js");
 const port = app.get("PORT");
 
-app.listen(port, () => console.log(`Listen on port ${port}`));
+conn.sync({ force: true }).then(() => {
+  app.listen(port, () => console.log(`Listen on port ${port}`));
+});
--- api/src/models/transaction.js
+++ api/src/models/transaction.js
@@ -1,1 +1,34 @@
-console.log("Se crea archivo en models/transaction.js");
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "transaction",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      concept: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.ENUM,
+        values: ["deposit", "withdrawal"],
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
