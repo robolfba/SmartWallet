7d0771c52d2dc8710b2ab0238ff0bcaf15fe4e38
unknown <robolfba@gmail.com>
2021-11-30 13:18:00 -0300

    Se modifica force:false para conservar info en la base de datos

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@ const app = require("./config/server.js");
 const { conn } = require("./src/db.js");
 const port = app.get("PORT");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   app.listen(port, () => console.log(`Listen on port ${port}`));
 });
